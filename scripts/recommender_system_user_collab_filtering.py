bb300e786143a64fe55bd4cab0be3c8a327fee81
paramanandnarsinhabhat <141020967+paramanandnarsinhabhat@users.noreply.github.com>
2024-01-08 11:59:49 +0530

    commit reading files

--- scripts/recommender_system_user_collab_filtering.py
+++ scripts/recommender_system_user_collab_filtering.py
@@ -4,3 +4,13 @@ import pandas as pd
 import numpy as np
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
+
+
+#Reading train file
+
+train_df = pd.read_csv('/Users/paramanandbhat/Downloads/Article_Recommendation 2/train.csv')
+
+print(train_df.head())
+print(train_df.columns)
+
+
