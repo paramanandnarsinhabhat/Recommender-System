2b47b5db7dd011280b65ab6102b8edbd2ea377d0
paramanandnarsinhabhat <141020967+paramanandnarsinhabhat@users.noreply.github.com>
2024-01-08 16:02:49 +0530

    Update recommender_system_user_collab_filtering.py

--- scripts/recommender_system_user_collab_filtering.py
+++ scripts/recommender_system_user_collab_filtering.py
@@ -31,3 +31,26 @@ print(test_df.head())
 print(test_df.columns)
 
 
+#Step 1: Data Preparation
+'''We'll start by merging the article_info dataset with the
+ train and test datasets. This is to ensure that the article details 
+ are incorporated into the model training and evaluation process.'''
+
+
+# Merging article information with the training and test datasets
+
+train_df = train_df.merge(article_info_df, on='article_id', how='left')
+
+print(train_df.columns)
+
+test_df = test_df.merge(article_info_df, on='article_id', how='left')
+
+print(test_df.columns)
+
+# Displaying the first few rows of the merged training and test datasets
+merged_data_samples = {
+    "Merged Training Data": train_df.head(),
+    "Merged Test Data": test_df.head()
+}
+
+print(merged_data_samples)
